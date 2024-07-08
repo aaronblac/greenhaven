0dfec33a3d78f69b52f6c31180d78b66acd2cfd9
aaron <aaronblac@gmail.com>
2024-07-07 17:20:57 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GreenHaven',
+  webDir: 'dist'
+};
+
+export default config;
