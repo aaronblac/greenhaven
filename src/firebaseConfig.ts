44a2f19573702da134a68dc9a3a1f9becbbee43d
aaron <aaronblac@gmail.com>
2024-07-09 20:04:40 -0700

    added firebase functions and hosting

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,15 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 
+import 'dotenv/config';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB_Y_Qe3MxkMm6m-RcxldPKbgDpFiYezIA",
-    authDomain: "greenhaven-d11b5.firebaseapp.com",
-    projectId: "greenhaven-d11b5",
-    storageBucket: "greenhaven-d11b5.appspot.com",
-    messagingSenderId: "728466169091",
-    appId: "1:728466169091:web:327bd557543e774dc1dd36",
-    measurementId: "G-0MHY66NPSF"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
